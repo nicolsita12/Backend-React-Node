5a3a17d7d18fa10c60761614077db767af4c1544
nicolsita12 <114593458+nicolsita12@users.noreply.github.com>
2024-03-31 19:29:48 -0500

    modified: user.js, package

--- NodeJs/models/user.js
+++ NodeJs/models/user.js
@@ -1,45 +1,41 @@
 const db = require('../config/config');
-
-
+const bcrypt = require('bcryptjs');
 const User = {};
 User.create = async (user, result) => {
-        const sql =
-            `INSERT INTO users (
-            email,
-            name,
-            lastname,
-            phone,
-            image,
-            password,
-            created_at,
-            updated_at
-            )
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
-
-        db.query(sql,
-            [
-                user.email,
-                user.name,
-                user.lastname,
-                user.phone,
-                user.image,
-                user.password,
-                new Date(),
-                new Date()
-            ], (err, res) => {
-                if (err) {
-                    console.log('Error al crear el usuario: ', err);
-                    result(err, null);
-                }
-                else {
-                console.log('Usuario creado: ', { id: res.insertId, ...user
-});
-                result(null, { id: res.insertId, ...user });
-            }
-        }
-    );
+const hash = await bcrypt.hash(user.password, 10);
+const sql =
+`INSERT INTO users (
+email,
+name,
+lastname,
+phone,
+image,
+password,
+created_at,
+updated_at
+)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+db.query(sql,
+[
+user.email,
+user.name,
+user.lastname,
+user.phone,
+user.image,
+hash,
+new Date(),
+new Date()
+], (err, res) => {
+if (err) {
+console.log('Error al crear el usuario: ', err);
+result(err, null);
 }
-
+else {
+console.log('Usuario creado: ', { id: res.insertId, ...user
+});
+result(null, { id: res.insertId, ...user });
+}
+}
+);
+}
 module.exports = User;
-
-module.exports = User;
